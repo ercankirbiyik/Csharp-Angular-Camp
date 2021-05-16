92ef6e321a47a320f434ef5c73fdd871de6381f2
rcnon <69839627+rcnon@users.noreply.github.com>
2021-05-17 02:32:16 +0300

    GenericsIntro

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> Names = new MyList<string>();
+            Names.Add("Ercan");
+            Console.WriteLine(Names.Length);
+
+            Names.Add("Engin");
+            Console.WriteLine(Names.Length);
+
+            foreach (var name in Names.Items)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
