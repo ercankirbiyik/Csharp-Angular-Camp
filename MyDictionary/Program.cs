38d5afda00f7b3b000f65888e9027527f5bca21e
rcnon <69839627+rcnon@users.noreply.github.com>
2021-05-21 00:07:40 +0300

    C#-Camp-Day4Hw5

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using DocumentFormat.OpenXml.ExtendedProperties;
+using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> Words = new MyDictionary<string>();
+            Words.Add("Word");
+            Console.WriteLine(Words.Length);
+
+            Words.Add("Class");
+            Console.WriteLine(Words.Length);
+
+            Words.Add("Anything");
+            Console.WriteLine(Words.Length);
+
+
+            foreach (var word in Words.Items)
+            {
+                Console.WriteLine(word);
+            }
+
+
+        }
+    }
+}
